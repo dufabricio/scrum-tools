18cd9ebce7a11461b5bc7877621e93e3ad21c969
dufabricio <edu.fabricio@gmail.com>
2017-11-03 17:48:52 +0000

    Hello Wolrd

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
 @RestController
